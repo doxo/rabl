47ef121e857129250287551fee1a8204456542d4
Tyler Ritchie <tyler.ritchie@doxo.com>
2016-12-12 13:42:16 -0800

    Update engine to allow compatibility with newer versions of multi_json, add dependencies necessary to run tests.

--- lib/rabl/engine.rb
+++ lib/rabl/engine.rb
@@ -157,7 +157,9 @@ module Rabl
       if Rabl.configuration.json_engine == :default
         json_output = json_output.to_json unless json_output.is_a?(String)
       else
-        json_output = Rabl.configuration.json_engine.encode(json_output) unless json_output.is_a?(String)
+        json_engine = Rabl.configuration.json_engine
+        json_method = json_engine.respond_to?(:dump) ? 'dump' : 'encode' # multi_json compatibility
+        json_output = json_engine.send(json_method, json_output) unless json_output.is_a?(String)
       end
       use_callback = Rabl.configuration.enable_json_callbacks && request_params[:callback].present?
       use_callback ? "#{request_params[:callback]}(#{json_output})" : json_output
